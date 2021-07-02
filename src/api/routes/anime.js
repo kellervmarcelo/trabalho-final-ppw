dabfd4a199175dac38e81fa341193323147be805
Marcelo Keller <marcelokv123@gmail.com>
2021-07-01 22:12:09 -0300

    adicionado crud anime

--- src/api/routes/anime.js
+++ src/api/routes/anime.js
@@ -10,45 +10,45 @@ route.get("/", async function (req, res) {
   let docs = await Anime.find().populate('autor')
   res.json(docs);
 });
-// //pesquisa por id
-// route.get("/albuns/:id", async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     let album = await Album.findById(id).populate('artista')
-//     res.json(album);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Erro ao pesquisar album" });
-//   }
-// });
-// //post
-// route.post("/albuns", async (req, res) => {
-//   try {
-//     const album = new Album(req.body);
-//     const resultado = await album.save();
-//     res.json(resultado);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ error: "Erro na hora de criar o album" });
-//   }
-// });
-// //update
-// route.put('/albuns/:id', async(req,res) => {
-//     const id = req.params.id
-//     const album = req.body
-//     const resultado = await Album.findByIdAndUpdate(id, album)
-//     res.json(resultado);
-// })
-// //delete
-// route.delete('/albuns/:id', async(req,res) =>{
-//     try {
-//         const id = req.params.id
-//         const resultado = await Album.findByIdAndDelete(id);
-//         res.send(resultado)
-//     } catch (error) {   
-//         console.error(error);
-//         res.status(500).send({ error: "Erro na hora de criar o album"})
-//     }
-// })
+//pesquisa por id
+route.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    let anime = await Anime.findById(id).populate('autor')
+    res.json(album);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao pesquisar anime" });
+  }
+});
+//post
+route.post("/", async (req, res) => {
+  try {
+    const anime = new Anime(req.body);
+    const resultado = await anime.save();
+    res.json(resultado);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Erro na hora de criar o album" });
+  }
+});
+//update
+route.put('/:id', async(req,res) => {
+    const id = req.params.id
+    const anime = req.body
+    const resultado = await Anime.findByIdAndUpdate(id, anime)
+    res.json(resultado);
+})
+//delete
+route.delete('/:id', async(req,res) =>{
+    try {
+        const id = req.params.id
+        const resultado = await Anime.findByIdAndDelete(id);
+        res.send(resultado)
+    } catch (error) {   
+        console.error(error);
+        res.status(500).send({ error: "Erro na hora de criar o album"})
+    }
+})
 
 module.exports = route;
